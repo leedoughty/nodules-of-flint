f72242160a5fdb361d42b8e8100e8b7ae10b367e
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-11 17:01:46 +0000

    Connect landing page to homepage

---
+++ pages/homepage.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const homepage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Nodules of Flint</title>
+      </Head>
+      <h1>Homepage</h1>
+    </div>
+  );
+};
+
+export default homepage;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
+export default function LandingPage() {
   return (
     <div>
       <Head>
@@ -11,7 +11,6 @@ export default function Home() {
           content="Brighton, Beach, Nodules of Flint"
         ></meta>
       </Head>
-      <h1>Nodules of Flint</h1>
       <Image
         src="/nodules-of-flint-animation.gif"
         alt="animation showing various flint rocks"
