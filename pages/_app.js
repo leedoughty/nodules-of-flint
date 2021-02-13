3fe5a497df93f77eb17414f4449029e1035fb185
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-13 15:04:53 +0000

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
