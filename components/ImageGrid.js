1fc92372aca194264b17e2a771f8a0d89f4b444b
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-22 17:41:37 +0000

    Randomise order of nodules of flint

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -1,17 +1,19 @@
 import homepageStyles from "../styles/Homepage.module.css";
+import _ from "lodash";
 
 const Grid = () => {
   const numberOfNodules = 74;
+  const randomisedNodulesArray = _.shuffle([...Array(numberOfNodules).keys()]);
 
   return (
     <section className={homepageStyles.container}>
-      {[...Array(numberOfNodules)].map((_, i) => {
+      {randomisedNodulesArray.map((number, i) => {
         return (
           <div className={homepageStyles["nodule-of-flint"]} key={i}>
             <img
               className={homepageStyles["nodule-image"]}
               alt="A nodule of flint"
-              src={`/nodules-of-flint/nodule-${i}.jpg`}
+              src={`/nodules-of-flint/nodule-${number}.jpg`}
             />
           </div>
         );
