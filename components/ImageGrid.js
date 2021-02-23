cca713a7d1f2bf4d4ac5af9af2d6b8f7002fe643
Lee Doughty <leedoughtydesign@gmail.com>
2021-02-23 20:33:09 +0000

    Add more nodules of flint

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -2,7 +2,7 @@ import homepageStyles from "../styles/Homepage.module.css";
 import _ from "lodash";
 
 const Grid = () => {
-  const numberOfNodules = 74;
+  const numberOfNodules = 90;
   const randomisedNodulesArray = _.shuffle([...Array(numberOfNodules).keys()]);
 
   return (
